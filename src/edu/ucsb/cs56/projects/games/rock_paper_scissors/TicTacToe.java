9e63bc787bf7698cba7583b161723813d26d081a
Andrew Ferguson <aferguson@umail.ucsb.edu>
2015-03-12 02:03:36 -0700

    color images on tictactoe tiles

--- src/edu/ucsb/cs56/projects/games/rock_paper_scissors/TicTacToe.java
+++ src/edu/ucsb/cs56/projects/games/rock_paper_scissors/TicTacToe.java
@@ -81,11 +81,13 @@ public class TicTacToe extends JFrame{
                     if (button[i].equals(e.getSource())){
                         if (sign%2 == 0){
                             button[i].setIcon(image1);
+                            button[i].setDisabledIcon(image1);
                             button[i].setEnabled(false);
                             isSet[i] = 1;
 			}
                     else{
 			button[i].setIcon(image2);
+			button[i].setDisabledIcon(image2);
                         button[i].setEnabled(false);
 			isSet[i] = 2;
 			}
